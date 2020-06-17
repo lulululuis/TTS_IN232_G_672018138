08b7c787f17f14f1514adf47783ab8a5d4e5d90b
unknown <luismauboy.lm@gmail.com>
2020-06-17 21:43:45 +0800

    Pola dalam

--- TTS_IN232_G_672018138/Main.cpp
+++ TTS_IN232_G_672018138/Main.cpp
@@ -9,11 +9,91 @@ void ketupat0() {
 	glEnd();
 }
 
+void ketupat0a() {
+	glBegin(GL_POLYGON);
+	glVertex2f(0, 10);
+	glVertex2f(19.5, 40);
+	glVertex2f(0, 70);
+	glEnd();
+}
+
+void ketupat0b() {
+	glBegin(GL_POLYGON);
+	glVertex2f(0, 20);
+	glVertex2f(13, 40);
+	glVertex2f(0, 60);
+	glEnd();
+}
+
+void ketupat0c() {
+	glBegin(GL_POLYGON);
+	glVertex2f(0, 30);
+	glVertex2f(6.5, 40);
+	glVertex2f(0, 50);
+	glEnd();
+}
+
+void ketupat0d() {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(0, 35);
+	glVertex2f(4, 40);
+	glVertex2f(0, 45);
+	glEnd();
+}
+
+void ketupat0e() {
+	glBegin(GL_POLYGON);
+	glVertex2f(0, 36.5);
+	glVertex2f(3, 40);
+	glVertex2f(0, 43.5);
+	glEnd();
+}
+
 void ketupat1() {
 	glBegin(GL_POLYGON);
 	glVertex2f(0, 0);
 	glVertex2f(25, 40);
 	glVertex2f(50, 0);
+	glEnd();
+}
+
+void ketupat1a() {
+	glBegin(GL_POLYGON);
+	glVertex2f(6.5, 0);
+	glVertex2f(25, 30);
+	glVertex2f(43.5, 0);
+	glEnd();
+}
+
+void ketupat1b() {
+	glBegin(GL_POLYGON);
+	glVertex2f(13, 0);
+	glVertex2f(25, 20);
+	glVertex2f(37, 0);
+	glEnd();
+}
+
+void ketupat1c() {
+	glBegin(GL_POLYGON);
+	glVertex2f(19.5, 0);
+	glVertex2f(25, 10);
+	glVertex2f(30.5, 0);
+	glEnd();
+}
+
+void ketupat1d() {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(22, 0);
+	glVertex2f(25, 5);
+	glVertex2f(28, 0);
+	glEnd();
+}
+
+void ketupat1e() {
+	glBegin(GL_POLYGON);
+	glVertex2f(23, 0);
+	glVertex2f(25, 3.5);
+	glVertex2f(27, 0);
 	glEnd();
 }
 
@@ -26,11 +106,96 @@ void ketupat2() {
 	glEnd();
 }
 
+void ketupat2a() {
+	glBegin(GL_POLYGON);
+	glVertex2f(31.5, 40);
+	glVertex2f(50, 10);
+	glVertex2f(68.5, 40);
+	glVertex2f(50, 70);
+	glEnd();
+}
+
+void ketupat2b() {
+	glBegin(GL_POLYGON);
+	glVertex2f(38, 40);
+	glVertex2f(50, 20);
+	glVertex2f(62, 40);
+	glVertex2f(50, 60);
+	glEnd();
+}
+
+void ketupat2c() {
+	glBegin(GL_POLYGON);
+	glVertex2f(44.5, 40);
+	glVertex2f(50, 30);
+	glVertex2f(55.5, 40);
+	glVertex2f(50, 50);
+	glEnd();
+}
+
+void ketupat2d() {
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(47, 40);
+	glVertex2f(50, 35);
+	glVertex2f(53, 40);
+	glVertex2f(50, 45);
+	glEnd();
+}
+
+void ketupat2e() {
+	glBegin(GL_POLYGON);
+	glVertex2f(48, 40);
+	glVertex2f(50, 36.5);
+	glVertex2f(52, 40);
+	glVertex2f(50, 43.5);
+	glEnd();
+}
+
 void ketupat3() {
 	glBegin(GL_POLYGON);
 	glVertex2f(50, 0);
 	glVertex2f(75, 40);
 	glVertex2f(100, 0);
+	glEnd();
+}
+
+void ketupat3a() {
+	glBegin(GL_POLYGON);
+	glVertex2f(56.5, 0);
+	glVertex2f(75, 30);
+	glVertex2f(93.5, 0);
+	glEnd();
+}
+
+void ketupat3b() {
+	glBegin(GL_POLYGON);
+	glVertex2f(63, 0);
+	glVertex2f(75, 20);
+	glVertex2f(87, 0);
+	glEnd();
+}
+
+void ketupat3c() {
+	glBegin(GL_POLYGON);
+	glVertex2f(69.5, 0);
+	glVertex2f(75, 10);
+	glVertex2f(80.5, 0);
+	glEnd();
+}
+
+void ketupat3d() {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(72, 0);
+	glVertex2f(75, 5);
+	glVertex2f(78, 0);
+	glEnd();
+}
+
+void ketupat3e() {
+	glBegin(GL_POLYGON);
+	glVertex2f(73, 0);
+	glVertex2f(75, 3.5);
+	glVertex2f(77, 0);
 	glEnd();
 }
 
@@ -42,10 +207,58 @@ void ketupat4() {
 	glEnd();
 }
 
+void ketupat4a() {
+	glBegin(GL_POLYGON);
+	glVertex2f(100, 10);
+	glVertex2f(81.5, 40);
+	glVertex2f(100, 70);
+	glEnd();
+}
+
+void ketupat4b() {
+	glBegin(GL_POLYGON);
+	glVertex2f(100, 20);
+	glVertex2f(88, 40);
+	glVertex2f(100, 60);
+	glEnd();
+}
+
+void ketupat4c() {
+	glBegin(GL_POLYGON);
+	glVertex2f(100, 30);
+	glVertex2f(94.5, 40);
+	glVertex2f(100, 50);
+	glEnd();
+}
+
+void ketupat4d() {
+	glBegin(GL_LINE_STRIP);
+	glVertex2f(100, 35);
+	glVertex2f(97, 40);
+	glVertex2f(100, 45);
+	glEnd();
+}
+
+void ketupat4e() {
+	glBegin(GL_POLYGON);
+	glVertex2f(100, 36.5);
+	glVertex2f(98, 40);
+	glVertex2f(100, 43.5);
+	glEnd();
+}
+
 void ketupat5() {
 	glBegin(GL_POLYGON);
 	glVertex2f(0, 80);
 	glVertex2f(25, 120);
+	glVertex2f(0, 120);
+	glEnd();
+}
+
+void ketupat5a() {
+	glBegin(GL_POLYGON);
+	glVertex2f(0, 90);
+	glVertex2f(19.5, 120);
 	glVertex2f(0, 120);
 	glEnd();
 }
@@ -59,11 +272,64 @@ void ketupat6() {
 	glEnd();
 }
 
+void ketupat6a() {
+	glBegin(GL_POLYGON);
+	glVertex2f(25, 50);
+	glVertex2f(6.5, 80);
+	glVertex2f(25, 110);
+	glVertex2f(43.5, 80);
+	glEnd();
+}
+
+void ketupat6b() {
+	glBegin(GL_POLYGON);
+	glVertex2f(25, 60);
+	glVertex2f(13, 80);
+	glVertex2f(25, 100);
+	glVertex2f(37, 80);
+	glEnd();
+}
+
+void ketupat6c() {
+	glBegin(GL_POLYGON);
+	glVertex2f(25, 70);
+	glVertex2f(19.5, 80);
+	glVertex2f(25, 90);
+	glVertex2f(30.5, 80);
+	glEnd();
+}
+
+void ketupat6d() {
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(25, 75);
+	glVertex2f(22, 80);
+	glVertex2f(25, 85);
+	glVertex2f(28, 80);
+	glEnd();
+}
+
+void ketupat6e() {
+	glBegin(GL_POLYGON);
+	glVertex2f(25, 76.5);
+	glVertex2f(23, 80);
+	glVertex2f(25, 83.5);
+	glVertex2f(27, 80);
+	glEnd();
+}
+
 void ketupat7() {
 	glBegin(GL_POLYGON);
 	glVertex2f(25, 120);
 	glVertex2f(50, 80);
 	glVertex2f(75, 120);
+	glEnd();
+}
+
+void ketupat7a() {
+	glBegin(GL_POLYGON);
+	glVertex2f(31.5, 120);
+	glVertex2f(50, 90);
+	glVertex2f(68.5, 120);
 	glEnd();
 }
 
@@ -76,10 +342,63 @@ void ketupat8() {
 	glEnd();
 }
 
+void ketupat8a() {
+	glBegin(GL_POLYGON);
+	glVertex2f(75, 50);
+	glVertex2f(56.5, 80);
+	glVertex2f(75, 110);
+	glVertex2f(93.5, 80);
+	glEnd();
+}
+
+void ketupat8b() {
+	glBegin(GL_POLYGON);
+	glVertex2f(75, 60);
+	glVertex2f(63, 80);
+	glVertex2f(75, 100);
+	glVertex2f(87, 80);
+	glEnd();
+}
+
+void ketupat8c() {
+	glBegin(GL_POLYGON);
+	glVertex2f(75, 70);
+	glVertex2f(69.5, 80);
+	glVertex2f(75, 90);
+	glVertex2f(80.5, 80);
+	glEnd();
+}
+
+void ketupat8d() {
+	glBegin(GL_LINE_LOOP);
+	glVertex2f(75, 75);
+	glVertex2f(72, 80);
+	glVertex2f(75, 85);
+	glVertex2f(78, 80);
+	glEnd();
+}
+
+void ketupat8e() {
+	glBegin(GL_POLYGON);
+	glVertex2f(75, 76.5);
+	glVertex2f(73, 80);
+	glVertex2f(75, 83.5);
+	glVertex2f(77, 80);
+	glEnd();
+}
+
 void ketupat9() {
 	glBegin(GL_POLYGON);
 	glVertex2f(75, 120);
 	glVertex2f(100, 80);
+	glVertex2f(100, 120);
+	glEnd();
+}
+
+void ketupat9a() {
+	glBegin(GL_POLYGON);
+	glVertex2f(81.5, 120);
+	glVertex2f(100, 90);
 	glVertex2f(100, 120);
 	glEnd();
 }
@@ -114,10 +433,21 @@ void garis_bawah() {
 	glEnd();
 }
 
-void display() {
-	glClear(GL_COLOR_BUFFER_BIT);
+void garis() {
+	glLineWidth(10);
 
-	glColor3ub(255, 69, 0);
+	glColor3ub(0, 0, 0);
+	garis_atas();
+
+	glColor3ub(0, 0, 0);
+	garis_tengah();
+
+	glColor3ub(0, 0, 0);
+	garis_bawah();
+}
+
+void ketupat() {
+	glColor3ub(255, 140, 0);
 	ketupat0();
 	ketupat2();
 	ketupat4();
@@ -125,22 +455,98 @@ void display() {
 	ketupat7();
 	ketupat9();
 
-	glColor3ub(154, 205, 50);
+	glColor3ub(107, 142, 35);
 	ketupat6();
 	ketupat8();
 
 	glColor3ub(255, 215, 0);
 	ketupat1();
 	ketupat3();
+}
 
+void ketupata() {
+	glColor3ub(0, 206, 209);
+	ketupat0a();
+	ketupat2a();
+	ketupat4a();
+
+	glColor3ub(255, 0, 0);
+	ketupat1a();
+	ketupat3a();
+
+	glColor3ub(255, 215, 0);
+	ketupat6a();
+	ketupat8a();
+
+	glColor3ub(245, 245, 245);
+	ketupat5a();
+	ketupat7a();
+	ketupat9a();
+}
+
+void ketupatb() {
+	glColor3ub(245, 245, 245);
+	ketupat0b();
+	ketupat2b();
+	ketupat4b();
+
+	glColor3ub(154, 205, 50);
+	ketupat1b();
+	ketupat3b();
+
+	glColor3ub(220, 20, 60);
+	ketupat6b();
+	ketupat8b();
+}
+
+void ketupatc() {
 	glColor3ub(0, 0, 0);
-	garis_atas();
-	
-	glColor3ub(0, 0, 0);
-	garis_tengah();
-	
-	glColor3ub(0, 0, 0);
-	garis_bawah();
+	ketupat0c();
+	ketupat1c();
+	ketupat2c();
+	ketupat3c();
+	ketupat4c();
+	ketupat6c();
+	ketupat8c();
+}
+
+void ketupatd() {
+	glLineWidth(2);
+	glColor3ub(255, 0, 0);
+	ketupat0d();
+	ketupat2d();
+	ketupat4d();
+
+	glColor3ub(255, 105, 180);
+	ketupat1d();
+	ketupat3d();
+	ketupat6d();
+	ketupat8d();
+}
+
+void ketupate() {
+	glColor3ub(255, 215, 0);
+	ketupat0e();
+	ketupat2e();
+	ketupat4e();
+
+	glColor3ub(0, 206, 209);
+	ketupat6e();
+	ketupat8e();
+	ketupat1e();
+	ketupat3e();
+}
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	ketupat();
+	ketupata();
+	ketupatb();
+	ketupatc();
+	ketupatd();
+	ketupate();
+	garis();
 
 	glFlush();
 }
@@ -148,7 +554,6 @@ void display() {
 void myinit() {
 	glClearColor(1.0, 1.0, 1.0, 1.0);
 	glColor3f(1.0, 0.0, 0.0);
-	glLineWidth(10);
 	glPointSize(2.0);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
